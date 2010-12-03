dab7a929691d39068499fe559ec3f6728a92faf5
Cummingsj <Cummingsj@2fd44642-17f8-11df-8e9e-7b60df378584>
2010-12-03 23:31:33 +0000

    mysql write code, let's see what blows up

--- trunk/tipserver.pl
+++ trunk/tipserver.pl
@@ -42,7 +42,7 @@ my $dbpass = 'secret';
 # server goes away and throw a warn
 my $dbh = DBI->connect("DBI:mysql:database=$dbname;host=$dbhost",
 						"$dbuname","$dbpass",
-						{PrintError=>1, mysql_auto_reconnect => 1}) or carp DBI->errstr;
+						{PrintError=>1, mysql_auto_reconnect => 1, mysql_connect_timeout=>30}) or carp DBI->errstr;
 
 if($ARGV[0] && $ARGV[0] eq "DEBUG") { $IO::Socket::SSL::DEBUG = 1; } 
 
@@ -83,32 +83,34 @@ while (1) {
 			$remote_md5=$&;
 			$data_read=~s/^\w{32}//;
 			$md5sum = md5_hex( $data_read );
-			@d_read=unpack("I I L L L I I I I L L I I s s s s L L L s s a**",$data_read);
+			@d_read=unpack("I I L L L I I I I L L I I s s s s L L L s s a*",$data_read);
 			print "\n\n\n****EVENT DATA****\n",
-				"sensor_id:$d_read[0]\n",
-				"event_id:$d_read[1]\n",
-				"tv_sec:$d_read[2]\n",
-				"tv_usec:$d_read[3]\n",
-				"sig_id:$d_read[4]\n",
-				"sig_gen:$d_read[5]\n",
-				"sig_rev:$d_read[6]\n",
-				"class:$d_read[7]\n",
-				"pri:$d_read[8]\n",
-				"sip:$d_read[9]\n",
-				"dip:$d_read[10]\n",
-				"sp:$d_read[11]\n",
-				"dp:$d_read[12]\n",
-				"protocol:$d_read[13]\n",
-				"pkt_action:$d_read[14]\n",
+			    "sensor_name:$d_read[0]\n",
+			    "sensor_interface:$d_read[1]\n",
+				"sensor_id:$d_read[2]\n",
+				"event_id:$d_read[3]\n",
+				"tv_sec:$d_read[4]\n",
+				"tv_usec:$d_read[5]\n",
+				"sig_id:$d_read[6]\n",
+				"sig_gen:$d_read[7]\n",
+				"sig_rev:$d_read[8]\n",
+				"class:$d_read[9]\n",
+				"pri:$d_read[10]\n",
+				"sip:$d_read[11]\n",
+				"dip:$d_read[12]\n",
+				"sp:$d_read[13]\n",
+				"dp:$d_read[14]\n",
+				"protocol:$d_read[15]\n",
+				"pkt_action:$d_read[16]\n",
 				"****PACKET DATA****\n",
-				"sensor_id:$d_read[15]\n",
-				"event_id:$d_read[16]\n",
-				"tv_sec:$d_read[17]\n",
-				"pkt_sec:$d_read[18]\n",
-				"pkt_usec:$d_read[19]\n",
-				"linktype:$d_read[20]\n",
-				"pkt_len:$d_read[21]\n",
-				"pkt:\n".print_format_packet(pack("H*",$d_read[22])) if $tdebug;
+				"sensor_id:$d_read[17]\n",
+				"event_id:$d_read[18]\n",
+				"tv_sec:$d_read[19]\n",
+				"pkt_sec:$d_read[20]\n",
+				"pkt_usec:$d_read[21]\n",
+				"linktype:$d_read[22]\n",
+				"pkt_len:$d_read[23]\n",
+				"pkt:\n".print_format_packet(pack("H*",$d_read[24])) if $tdebug;
 			
 			print "remote md5: $remote_md5\n",
 				"local md5: $md5sum\n",
@@ -120,6 +122,62 @@ while (1) {
 			close($s);
 			undef $s;
 			print "connection closed.\n" if $tdebug;
+			print "Writing mysql Data\n" if $tdebug;
+					
+			# check for our sensor and interface values, create if they don't exist
+			my $sth = $dbh->prepare("SELECT sid, hostname FROM sensor where hostname = '$d_read[0]' and interface = '$d_read[1]'");
+			$sth->execute();
+			my @data = $sth->fetchrow_array();
+			my $sensorid=$data[0];
+			unless ($data[1]) {
+				$dbh->do("INSERT INTO sensor (hostname,interface,detail,encoding) VALUES ('$d_read[0]','$d_read[1]',1,0)") or errorkill($dbh->errstr);
+				$sth = $dbh->prepare("SELECT sid FROM sensor where hostname = '$d_read[0]' and interface = '$d_read[1]'");
+				$sth->execute();
+				$sensorid = $sth->fetchrow_array();
+			}
+			my $sid = getcid($sensorid);
+			
+			my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime($d_read[20]);
+			$year=$year+1900;
+			$mon=$mon+1;
+			
+			# Insert event and iphdr info (for every packet)
+			$dbh->do("INSERT INTO event (sid,cid,signature,timestamp) VALUES ('$sensorid','$cid','$d_read[6]','$year-$mon-$mday $hour:$min:$sec')") or errorkill($dbh->errstr);
+			$dbh->do("INSERT INTO iphdr (sid,cid,ip_src,ip_dst,ip_proto) VALUES ('$sensorid','$cid','$d_read[11]','$d_read[12]','$d_read[15]')") or errorkill($dbh->errstr);
+			
+			my $proto;
+			
+			# TCP packet info
+			if ($d_read[15] == 6){
+				$dbh->do("INSERT INTO tcphdr (sid,cid,tcp_sport,tcp_dport,tcp_flags) VALUES ('$sensorid','$cid','$d_read[13]','$d_read[14]',0)") or errorkill($dbh->errstr);
+				# tag hash population
+				$proto="TCP";
+			}
+			
+			# UDP packet info
+			elsif($d_read[15] == 17){
+				$dbh->do("INSERT INTO udphdr (sid,cid,udp_sport,udp_dport) VALUES ('$sensorid','$cid','$d_read[13]','$d_read[14]'')") or errorkill($dbh->errstr);
+				# tag hash population
+				$proto="UDP";
+			}
+			
+			# ICMP packet info
+			elsif($d_read[15] == 1){
+				$dbh->do("INSERT INTO icmphdr (sid,cid,icmp_type,icmp_code) VALUES ('$sensorid','$cid','$d_read[13]','$d_read[14]'')") or errorkill($dbh->errstr);
+				# tag hash population
+				$proto="ICMP";
+			}
+			
+			# Unsupported packet type by snort schema 107
+			else {
+				print "\n\n!!!!!!!!!!!!UNSUPPORTED PACKET TYPE: $d_read[15]\n\n" if $termdebug;
+			}		
+			
+			my $pkthex=unpack("H*", $d_read[24]);
+			# It's a regular packet associated with a regular event!
+			$dbh->do("INSERT INTO data (sid,cid,data_payload) VALUES ('$sensorid','$cid','$pkthex')") or errorkill($dbh->errstr);
+			$dbh->do("UPDATE sensor SET last_cid = '$cid' WHERE sid = '$sensorid'") or errorkill($dbh->errstr);
+			
 			exit(0);
 		}
 	}
@@ -148,6 +206,21 @@ sub start_sock {
 	    exit(0);
 	}else{ print "socket created: $sock.\n" if $tdebug; }
 	return $sock;
+}
+
+# Generate an updated cid
+sub getcid {
+	my $sensorid = shift;
+	my $sth = $dbh->prepare("SELECT MAX(cid) from event where sid = $sensorid");
+	$sth->execute();
+	if ($sth) {
+		$cid = $sth->fetchrow_array();
+		$cid++;
+	}else{
+		$cid = $feed{'event_id'};
+	}
+	print "\ncid: $cid" if $termdebug;
+	return $cid;
 }
 
 # Make packet debug output pretty!
